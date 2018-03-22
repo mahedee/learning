a6349d315191bfbd1e4a5eb5509e7efc88f3449e
mahedee <mahedee.hasan@gmail.com>
2018-03-22 16:31:20 +0600

    added some new projects

---
+++ Java-EE-Getting-Started/mypractice/arquillian-test/arquillian-tutorial/src/test/java/org/arquillian/example/GreeterTest.java
@@ -0,0 +1,25 @@
+package org.arquillian.example;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class GreeterTest {
+
+    @Deployment
+    public static JavaArchive deployService() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(Greeter.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+    @Test
+    public void shouldGreetTheWorld() throws Exception {
+        Assert.assertEquals("Hello, World!", "Hello, World!");
+    }
+}
---
+++ Java-EE-Getting-Started/mypractice/bookstore-back/src/test/java/com/pluralsight/bookstore/repository/BookRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.pluralsight.bookstore.repository;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(Arquillian.class)
+public class BookRepositoryTest {
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(BookRepository.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
+                .addAsManifestResource("META-INF/test-persistence.xml", "persistence.xml");
+
+    }
+
+    @Test
+    public void create() {
+    }
+}
