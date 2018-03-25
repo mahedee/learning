8af225e647ac59a428c08e648eea2d4451874e71
mahedee <mahedee.hasan@gmail.com>
2018-03-25 19:21:00 +0600

    update project

--- Java-EE-Getting-Started/mypractice/bookstore-back/src/main/java/com/pluralsight/bookstore/model/Language.java
+++
@@ -1,5 +0,0 @@
-package com.pluralsight.bookstore.model;
-
-public enum Language {
-    ENGLISH, BANGLA, HINDI, DUTCH, DANISH, ITALIAN
-}
---
+++ Java-EE-Getting-Started/mypractice/bookstore-back_old/src/main/java/com/pluralsight/bookstore/model/Language.java
@@ -0,0 +1,16 @@
+package com.pluralsight.bookstore.model;
+
+/**
+ * @author Mahedee Hasan
+ *         http://mahedee.net
+ *         --
+ */
+
+public enum Language {
+
+    // ======================================
+    // =             Constants              =
+    // ======================================
+
+    ENGLISH, FRENCH, SPANISH, PORTUGUESE, ITALIAN, FINISH, GERMAN, DEUTSCH, RUSSIAN
+}
