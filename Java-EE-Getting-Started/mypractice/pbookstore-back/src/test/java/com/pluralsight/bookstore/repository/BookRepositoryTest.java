9456ba55aad3c31795823167983781238536e345
mahedee <mahedee.hasan@gmail.com>
2018-03-27 14:20:40 +0600

    complete module 06

--- Java-EE-Getting-Started/mypractice/pbookstore-back/src/test/java/com/pluralsight/bookstore/repository/BookRepositoryTest.java
+++ Java-EE-Getting-Started/mypractice/pbookstore-back/src/test/java/com/pluralsight/bookstore/repository/BookRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.pluralsight.bookstore.repository;
 
+import com.pluralsight.bookstore.model.Book;
+import com.pluralsight.bookstore.model.Language;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -8,18 +10,62 @@ import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import javax.inject.Inject;
+
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 @RunWith(Arquillian.class)
 public class BookRepositoryTest {
+
+    @Inject
+    private BookRepository bookRepository;
+
     @Deployment
     public static JavaArchive createDeployment() {
         return ShrinkWrap.create(JavaArchive.class)
                 .addClass(BookRepository.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+                .addClass(Book.class)
+                .addClass(Language.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
+                .addAsManifestResource("META-INF/test-persistence.xml", "persistence.xml");
     }
 
     @Test
     public void create() {
+        //Test counting books
+        assertEquals(Long.valueOf(0), bookRepository.countAll());
+        assertEquals(0, bookRepository.findAll().size());
+
+        //Create book
+        Book book = new Book("isbn", "a title", 12F, 123, Language.ENGLISH, new Date(), "http://blab bala", "description");
+        book = bookRepository.create(book);
+
+        Long bookId = book.getId();
+
+        //Check create book
+        assertNotNull(book);
+
+        //Find created book
+        Book bookFound = bookRepository.find(bookId);
+
+        //check the book found
+        assertEquals("a title",bookFound.getTitle());
+
+        //Test counting books
+        assertEquals(Long.valueOf(1), bookRepository.countAll());
+        assertEquals(1, bookRepository.findAll().size());
+
+        //delete the book
+        bookRepository.delete(bookId);
+
+        //Test counting books
+        assertEquals(Long.valueOf(0), bookRepository.countAll());
+        assertEquals(0, bookRepository.findAll().size());
+
+
     }
+
+
 }
